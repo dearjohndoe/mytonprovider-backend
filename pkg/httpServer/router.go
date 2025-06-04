0b178ee05e3f81662dc46ee522122048ea30ada8
DearJohnDoe <>
2025-06-05 01:48:39 +0400

    inc limits

--- pkg/httpServer/router.go
+++ pkg/httpServer/router.go
@@ -4,12 +4,12 @@ func (h *handler) RegisterRoutes() {
 
 	apiv1 := h.server.Group("/api/v1", h.loggerMiddleware)
 	{
-		apiv1.Post("/providers/search", h.rateLimiterMiddleware("provicers", 10), h.searchProviders)
+		apiv1.Post("/providers/search", h.rateLimiterMiddleware("provicers", 60), h.searchProviders)
 
-		apiv1.Post("/providers", h.rateLimiterMiddleware("providers", 10), h.updateTelemetry)
+		apiv1.Post("/providers", h.rateLimiterMiddleware("providers", 60), h.updateTelemetry)
 
 		apiv1.Get("/providers", h.rateLimiterMiddleware("providers", 60), h.authorizationMiddleware, h.getLatestTelemetry)
 	}
 
-	h.server.Get("/health", h.rateLimiterMiddleware("health", 60), h.health)
+	h.server.Get("/health", h.rateLimiterMiddleware("health", 200), h.health)
 }
