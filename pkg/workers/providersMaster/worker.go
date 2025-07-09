eb1d8ba4c1197b98c95acd18ea6173e05330f717
DearJohnDoe <>
2025-07-09 21:26:24 +0300

    + to prew commit

--- pkg/workers/providersMaster/worker.go
+++ pkg/workers/providersMaster/worker.go
@@ -3,10 +3,12 @@ package providersmaster
 import (
 	"context"
 	"encoding/hex"
+	"fmt"
 	"log/slog"
 	"math/big"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/xssnick/tonutils-storage-provider/pkg/transport"
@@ -16,14 +18,18 @@ import (
 )
 
 const (
-	lastLTKey               = "masterWalletLastLT"
-	prefix                  = "tsp-"
-	fakeSize                = 1
-	providerResponseTimeout = 5 * time.Second
+	lastLTKey                     = "masterWalletLastLT"
+	prefix                        = "tsp-"
+	storageRewardWithdrawalOpCode = 0xa91baf56
+	fakeSize                      = 1
+	providerResponseTimeout       = 5 * time.Second
 )
 
 type providers interface {
 	GetAllProvidersPubkeys(ctx context.Context) (pubkeys []string, err error)
+	GetAllProvidersWallets(ctx context.Context) (wallets []db.ProviderWallet, err error)
+	UpdateProvidersLT(ctx context.Context, providers []db.ProviderWalletLT) (err error)
+	AddStorageContracts(ctx context.Context, contracts []db.StorageContract) (err error)
 	AddProviders(ctx context.Context, providers []db.ProviderCreate) (err error)
 	UpdateProviders(ctx context.Context, providers []db.ProviderUpdate) (err error)
 	AddStatuses(ctx context.Context, providers []db.ProviderStatusUpdate) (err error)
@@ -38,6 +44,8 @@ type system interface {
 
 type ton interface {
 	GetTransactions(ctx context.Context, addr string, lastProcessedLT uint64) (tx []*tonclient.Transaction, err error)
+	GetStorageContractsInfo(ctx context.Context, addrs []string) (contracts []tonclient.StorageContract, err error)
+	GetProvidersInfo(ctx context.Context, addrs []string) (contractsProviders []tonclient.StorageContractProviders, err error)
 }
 
 type providersMasterWorker struct {
@@ -53,6 +61,7 @@ type providersMasterWorker struct {
 type Worker interface {
 	CollectNewProviders(ctx context.Context) (interval time.Duration, err error)
 	UpdateKnownProviders(ctx context.Context) (interval time.Duration, err error)
+	CollectProvidersNewStorageContracts(ctx context.Context) (interval time.Duration, err error)
 	UpdateUptime(ctx context.Context) (interval time.Duration, err error)
 	UpdateRating(ctx context.Context) (interval time.Duration, err error)
 }
@@ -244,6 +253,129 @@ func (w *providersMasterWorker) UpdateKnownProviders(ctx context.Context) (inter
 	return
 }
 
+func (w *providersMasterWorker) CollectProvidersNewStorageContracts(ctx context.Context) (interval time.Duration, err error) {
+	const (
+		successInterval = 30 * time.Minute
+		failureInterval = 15 * time.Second
+	)
+
+	log := w.logger.With("worker", "ProvidersContracts")
+	log.Debug("collect new providers contracts")
+
+	interval = successInterval
+
+	providersWallets, err := w.providers.GetAllProvidersWallets(ctx)
+	if err != nil {
+		interval = failureInterval
+		return
+	}
+
+	providersToUpdate := make([]db.ProviderWalletLT, 0, len(providersWallets))
+	storageContracts := make(map[string]db.StorageContract)
+
+	wg := sync.WaitGroup{}
+	smu := sync.Mutex{}
+	pmu := sync.Mutex{}
+
+	wg.Add(len(providersWallets))
+	for _, provider := range providersWallets {
+		go func(ctx context.Context) {
+			defer wg.Done()
+
+			// TODO: retries on errors
+			var lastLT uint64
+			sc, lastLT, err := w.scanProviderTransactions(ctx, provider)
+			if err != nil {
+				log.Error("failed to scan provider transactions", "address", provider.Address, "error", err)
+			}
+
+			smu.Lock()
+			for src, tx := range sc {
+				if v, ok := storageContracts[src]; ok {
+					for p := range tx.ProvidersAddresses {
+						v.ProvidersAddresses[p] = struct{}{}
+					}
+					if v.LastLT < tx.LastLT {
+						v.LastLT = tx.LastLT
+					}
+					storageContracts[src] = v
+				} else {
+					storageContracts[src] = tx
+				}
+			}
+			smu.Unlock()
+
+			if lastLT != provider.LT {
+				pmu.Lock()
+				providersToUpdate = append(providersToUpdate, db.ProviderWalletLT{
+					PubKey: provider.PubKey,
+					LT:     lastLT,
+				})
+				pmu.Unlock()
+			}
+		}(ctx)
+	}
+
+	wg.Wait()
+
+	if len(storageContracts) == 0 {
+		return
+	}
+
+	// Collect more info about storage contracts
+	contractsAdresses := make([]string, 0, len(storageContracts))
+	for address := range storageContracts {
+		contractsAdresses = append(contractsAdresses, address)
+	}
+
+	contractsInfo, err := w.ton.GetStorageContractsInfo(ctx, contractsAdresses)
+	if err != nil {
+		log.Error("failed to get storage contracts info", "error", err)
+		interval = failureInterval
+		return
+	}
+
+	newContracts := make([]db.StorageContract, 0, len(contractsInfo))
+	for _, contract := range contractsInfo {
+		if _, ok := storageContracts[contract.Address]; !ok {
+			log.Error("storage contract not found in scanned transactions", "address", contract.Address)
+			continue
+		}
+
+		newContracts = append(newContracts, db.StorageContract{
+			ProvidersAddresses: storageContracts[contract.Address].ProvidersAddresses,
+			Address:            contract.Address,
+			BagID:              contract.BagID,
+			OwnerAddr:          contract.OwnerAddr,
+			Size:               contract.Size,
+			ChunkSize:          contract.ChunkSize,
+			LastLT:             storageContracts[contract.Address].LastLT,
+		})
+
+		if contract.Size == 0 || contract.ChunkSize == 0 {
+			log.Error("invalid storage contract size or chunk size", "address", contract.Address, "size", contract.Size, "chunkSize", contract.ChunkSize)
+			continue
+		}
+
+	}
+
+	err = w.providers.UpdateProvidersLT(ctx, providersToUpdate)
+	if err != nil {
+		log.Error("failed to update providers wallets lt", "error", err)
+		interval = failureInterval
+		return
+	}
+
+	err = w.providers.AddStorageContracts(ctx, newContracts)
+	if err != nil {
+		log.Error("failed to add storage contracts", "error", err)
+		interval = failureInterval
+		return
+	}
+
+	return
+}
+
 func (w *providersMasterWorker) UpdateUptime(ctx context.Context) (interval time.Duration, err error) {
 	const (
 		successInterval = 5 * time.Minute
@@ -279,6 +411,45 @@ func (w *providersMasterWorker) UpdateRating(ctx context.Context) (interval time
 	if err != nil {
 		interval = failureInterval
 		return
+	}
+
+	return
+}
+
+func (w *providersMasterWorker) scanProviderTransactions(ctx context.Context, provider db.ProviderWallet) (contracts map[string]db.StorageContract, lastLT uint64, err error) {
+	timeoutCtx, cancel := context.WithTimeout(ctx, 20*time.Second)
+	defer cancel()
+
+	txs, err := w.ton.GetTransactions(timeoutCtx, provider.Address, provider.LT)
+	if err != nil {
+		err = fmt.Errorf("failed to get transactions error: %w", err)
+		return
+	}
+
+	contracts = make(map[string]db.StorageContract, len(txs))
+
+	lastLT = provider.LT
+	for _, tx := range txs {
+		if tx == nil {
+			continue
+		}
+
+		if tx.Op != storageRewardWithdrawalOpCode {
+			continue
+		}
+
+		s := db.StorageContract{
+			ProvidersAddresses: make(map[string]struct{}),
+			Address:            tx.From,
+			LastLT:             tx.LT,
+		}
+		s.ProvidersAddresses[provider.Address] = struct{}{}
+
+		if tx.LT > lastLT {
+			lastLT = tx.LT
+		}
+
+		contracts[tx.From] = s
 	}
 
 	return
